155a22bd53a309ed5f299e0aa2c40e7610435f7e
songlin <songlin09@qq.com>
2021-03-30 22:07:30 +0800

    RoomDemo 插入和读取数据 (协程)

--- app/src/main/java/com/sl/app/MainActivity.kt
+++ app/src/main/java/com/sl/app/MainActivity.kt
@@ -3,37 +3,61 @@ package com.sl.app
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.sl.app.db.AppDatabase
 import com.sl.app.entity.User
-import kotlin.concurrent.thread
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        thread {
+//        thread {
+//            val db = Room.databaseBuilder(
+//                    applicationContext,
+//                    AppDatabase::class.java, "database_name"
+//            ).build()
+//
+//            val users = mutableListOf<User>().apply {
+//                add(User(0, "chen", "sl"))
+//                add(User(0, "chen1", "sl1"))
+//                add(User(0, "chen2", "sl2"))
+//                add(User(0, "chen3", "sl3"))
+//            }
+//
+//            db.userDao().insertAll(*users.toTypedArray())
+//
+//            var all = db.userDao().getAll()
+//
+//            runOnUiThread {
+//                findViewById<TextView>(R.id.textView).text = all.toString()
+//            }
+//
+//        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
             val db = Room.databaseBuilder(
                     applicationContext,
                     AppDatabase::class.java, "database_name"
             ).build()
 
-            val users = mutableListOf<User>().apply {
-                add(User(0, "chen", "sl"))
-                add(User(0, "chen1", "sl1"))
-                add(User(0, "chen2", "sl2"))
-                add(User(0, "chen3", "sl3"))
+            val  getUsers = withContext(Dispatchers.IO){
+                val users = mutableListOf<User>().apply {
+                    add(User(0, "chen", "sl"))
+                    add(User(0, "chen1", "sl1"))
+                    add(User(0, "chen2", "sl2"))
+                    add(User(0, "chen3", "sl3"))
+                }
+                db.userDao().insertAll(*users.toTypedArray())
+                db.userDao().getAll()
             }
 
-            db.userDao().insertAll(*users.toTypedArray())
-
-            var all = db.userDao().getAll()
-
-            runOnUiThread {
-                findViewById<TextView>(R.id.textView).text = all.toString()
-            }
-
+            findViewById<TextView>(R.id.textView).text = getUsers.toString()
         }
+
     }
 }
